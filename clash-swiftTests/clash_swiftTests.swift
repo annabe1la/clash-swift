b76951f41e2846dc4eaea3bcfe66ea4d971bffb7
annabe1la <745283555@qq.com>
2026-08-04 08:55:28 +0800

    Initial Commit

---
+++ clash-swiftTests/clash_swiftTests.swift
@@ -0,0 +1,19 @@
+//
+//  clash_swiftTests.swift
+//  clash-swiftTests
+//
+//  Created by AnNabe1la on 2026/8/4.
+//
+
+import Testing
+@testable import clash_swift
+
+struct clash_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
---
+++ clash-swiftUITests/clash_swiftUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  clash_swiftUITestsLaunchTests.swift
+//  clash-swiftUITests
+//
+//  Created by AnNabe1la on 2026/8/4.
+//
+
+import XCTest
+
+final class clash_swiftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
